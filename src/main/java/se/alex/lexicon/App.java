4392bed9261e90866e8507c2f8517a4b122a3c33
FullStackOS <alex.youssef@live.com>
2024-07-11 13:13:21 +0200

    Update App.java to correctly use singleton instances of DOAs

--- src/main/java/se/alex/lexicon/App.java
+++ src/main/java/se/alex/lexicon/App.java
@@ -4,39 +4,49 @@ import se.alex.lexicon.dao.impl.AppUserDAOImpl;
 import se.alex.lexicon.dao.impl.PersonDAOImpl;
 import se.alex.lexicon.dao.impl.TodoItemDAOImpl;
 import se.alex.lexicon.dao.impl.TodoItemTaskDAOImpl;
-import se.alex.lexicon.dao.impl.sequencers.AppUserIdSequencer;
-import se.alex.lexicon.dao.impl.sequencers.PersonIdSequencer;
-import se.alex.lexicon.dao.impl.sequencers.TodoItemIdSequencer;
-import se.alex.lexicon.dao.impl.sequencers.TodoItemTaskIdSequencer;
-import se.alex.lexicon.model.*;
-import se.alex.lexicon.sequencers.*;
+import se.alex.lexicon.dao.impl.sequencer.AppUserIdSequencer;
+import se.alex.lexicon.dao.impl.sequencer.PersonIdSequencer;
+import se.alex.lexicon.dao.impl.sequencer.TodoItemIdSequencer;
+import se.alex.lexicon.dao.impl.sequencer.TodoItemTaskIdSequencer;
+import se.alex.lexicon.model.AppUser;
+import se.alex.lexicon.model.AppRole;
+import se.alex.lexicon.model.Person;
+import se.alex.lexicon.model.TodoItem;
+import se.alex.lexicon.model.TodoItemTask;
+
 import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
-        // Create DAO instances
-        se.alex.lexicon.dao.AppUserDAO appUserDAO = new AppUserDAOImpl();
-        se.alex.lexicon.dao.PersonDAO personDAO = new PersonDAOImpl();
-        se.alex.lexicon.dao.TodoItemDAO todoItemDAO = new TodoItemDAOImpl();
-        se.alex.lexicon.dao.TodoItemTaskDAO todoItemTaskDAO = new TodoItemTaskDAOImpl();
+        // Initializing DAOs
+        AppUserDAOImpl appUserDAO = AppUserDAOImpl.getInstance();
+        PersonDAOImpl personDAO = PersonDAOImpl.getInstance();
+        TodoItemDAOImpl todoItemDAO = TodoItemDAOImpl.getInstance();
+        TodoItemTaskDAOImpl todoItemTaskDAO = TodoItemTaskDAOImpl.getInstance();
 
-        // Create and persist some objects
-        AppUser appUser = new AppUser(AppUserIdSequencer.nextId(), "username", "password", "email@example.com", AppRole.USER);
-        appUserDAO.persist(appUser);
-
-        Person person = new Person(PersonIdSequencer.nextId(), "First", "Last", "email@example.com");
-        personDAO.persist(person);
-
+        // Creating sample data
+        AppUser appUser = new AppUser(AppUserIdSequencer.nextId(), "alex", "password123", "alex.lexicon@example.com", AppRole.USER);
+        Person person = new Person(PersonIdSequencer.nextId(), "Alex", "Lexicon", "alex.lexicon@example.com");
         TodoItem todoItem = new TodoItem(TodoItemIdSequencer.nextId(), "Title", "Description", LocalDate.now(), person);
-        todoItemDAO.persist(todoItem);
-
         TodoItemTask todoItemTask = new TodoItemTask(TodoItemTaskIdSequencer.nextId(), todoItem, person, true);
+
+        // Persisting sample data
+        appUserDAO.persist(appUser);
+        personDAO.persist(person);
+        todoItemDAO.persist(todoItem);
         todoItemTaskDAO.persist(todoItemTask);
 
-        // Print out some data
-        System.out.println("All AppUsers: " + appUserDAO.findAll());
-        System.out.println("All Persons: " + personDAO.findAll());
-        System.out.println("All TodoItems: " + todoItemDAO.findAll());
-        System.out.println("All TodoItemTasks: " + todoItemTaskDAO.findAll());
+        // Retrieving and printing data
+        System.out.println("AppUsers:");
+        appUserDAO.findAll().forEach(System.out::println);
+
+        System.out.println("Persons:");
+        personDAO.findAll().forEach(System.out::println);
+
+        System.out.println("TodoItems:");
+        todoItemDAO.findAll().forEach(System.out::println);
+
+        System.out.println("TodoItemTasks:");
+        todoItemTaskDAO.findAll().forEach(System.out::println);
     }
 }
